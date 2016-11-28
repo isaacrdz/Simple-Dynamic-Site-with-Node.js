f89a0e9c232f0b4791aa7f8c40a1d86b3617acac
isaacrdz <zack_cpp@hotmail.com>
2016-11-28 00:13:09 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+var router = require("./router.js");
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+ router.home(req,res);
+ router.user(req,res);
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
---
+++ router.js
@@ -0,0 +1,74 @@
+
+var Profile = require("./profile.js");
+var renderer = require("./renderer.js");
+var querystring = require("querystring");
+var commonHeaders = {'Content-Type': 'text/html'};
+
+function home(req, res){
+	if( req.url === '/'){
+		if(req.method.toLowerCase() === "get"){
+			res.writeHead(200, commonHeaders);
+			renderer.view("header",{}, res);
+			renderer.view("search",{}, res);
+			renderer.view("footer",{}, res);
+			res.end();
+		} else {
+			//if the url == "/" && POST
+			//get the post data from body
+			req.on("data", function(postBody){
+			//extract the username
+			var query = querystring.parse(postBody.toString());
+			//redirect to /:username
+			res.writeHead(303,{"Location":"/" + query.username});
+			res.end();
+			});
+		}
+	}
+}
+
+function user(req, res){
+	var username = req.url.replace("/", "");
+	if(username.length > 0 ){
+		res.writeHead(200, commonHeaders);
+	  	renderer.view("header",{}, res);
+
+
+
+	  	//get JSON from treehouse
+	  	var studentProfile = new Profile(username);
+	  	//on the end
+	  	studentProfile.on("end", function(profileJSON){
+	  		//show Profile
+
+	  		//Store value which we need
+	  		var values = {
+	  			avatarUrl: profileJSON.gravatar_url,
+	  			username: profileJSON.profile_name,
+	  			badges: profileJSON.badges.length,
+	  			javascriptPoints: profileJSON.points.JavaScript
+	  		}
+
+	  		//Simple response
+		  	renderer.view("profile",values, res);
+		  	renderer.view("footer",{}, res);
+		  	res.end();
+
+
+	  	});
+
+	  	//on error
+	  	studentProfile.on("error", function(error){
+	  		//show error
+        	renderer.view("error",{errorMessage: error.message}, res);
+	  		renderer.view("search",{}, res);
+			renderer.view("footer",{}, res);
+			res.end();
+
+		});
+	}
+}
+
+module.exports.home = home;
+module.exports.user = user;
+
+
